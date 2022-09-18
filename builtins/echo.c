9f3906a0d2148877059103c17957bd25706ad2b8
Knosper <79541185+Knosper@users.noreply.github.com>
2022-09-18 14:59:38 +0200

    Add files via upload

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,19 +6,42 @@
 /*   By: jjesberg <jjesberg@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 14:27:03 by jjesberg          #+#    #+#             */
-/*   Updated: 2022/09/15 15:40:55 by jjesberg         ###   ########.fr       */
+/*   Updated: 2022/09/17 21:24:39 by jjesberg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/minishell.h"
 
-int	echo(char *s)
+int	pipe_pos(char *s)
 {
-	if (!s)
+	int i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == '|')
+			return (i + 6);
+		i++;
+	}
+	return (5);
+}
+
+int	echo(t_data *data)
+{
+	int	i;
+	char *s;
+
+	s = data->tokens->content;
+	if (!s || !s[5])
 	{
 		printf("\n");
 		return (EXIT_SUCCESS);
 	}
-	printf("%s\n", s);
+	i = 5;
+	while (s[i] != NULL)
+	{
+		printf("%c", s[i++]);
+	}
+	printf("\n");
 	return (EXIT_SUCCESS);
 }
--- builtins/mini_exit.c
+++ builtins/mini_exit.c
@@ -1,24 +1,47 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exit.c                                             :+:      :+:    :+:   */
+/*   mini_exit.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jjesberg <jjesberg@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 14:28:23 by jjesberg          #+#    #+#             */
-/*   Updated: 2022/09/15 15:53:37 by jjesberg         ###   ########.fr       */
+/*   Updated: 2022/09/18 14:56:38 by jjesberg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/minishell.h"
 
+int	check_exit_args(t_data *data)
+{
+	t_token *tmp;
+
+	tmp = data->tokens;
+	if (data->builtins->command[1])
+	{
+		if (data->builtins->command[3])
+		{
+			print_error(6);
+			return (1);
+		}
+		// here = check if numeric value or not weird behavior exit has
+	}
+	return (0);
+}
+
 int	mini_exit(t_data *data)
 {
-	data->exit_minishell = true;
-	data->status = 815;
-	//unset Variables? 
-	//or more?
-	//freestuff;
-	printf("exit minishell\n");
+	//printf("data cmd = %s\n", data->builtins->command[0]);
+	/*if (data->builtins->command[1])
+	{
+		data->exit_minishell = check_exit_arg(data->builtins->command[1]);
+	}
+	if ((!tmp->next && !data->builtins->command[2]))
+		data->exit_minishell = true;*/
+	if (!data->tokens->next && !check_exit_args(data))
+	{
+		write(2, "exit minishell\n", 15);
+		data->exit_minishell = true;
+	}
 	return (EXIT_SUCCESS);
 }
