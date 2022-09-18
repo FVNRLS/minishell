f23c0b6d9cd332a710710f2e9bfdae46c715f5d9
Knosper <79541185+Knosper@users.noreply.github.com>
2022-09-18 15:00:49 +0200

    Add files via upload

--- src/executer/executer.c
+++ src/executer/executer.c
@@ -3,16 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   executer.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rmazurit <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jjesberg <jjesberg@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 12:47:27 by rmazurit          #+#    #+#             */
-/*   Updated: 2022/09/16 12:51:57 by rmazurit         ###   ########.fr       */
+/*   Updated: 2022/09/18 14:56:35 by jjesberg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/minishell.h"
 
+/*
+errrors in message printer && define macros for error in errror.h
+*/
 void	exec_commands(t_data *data)
 {
+	t_token *tmp;
+	int		mod;
+
 	data->exec_error = false;
+	tmp = (t_token*)data->tokens;
+	if (tmp)
+	{
+		mod = ft_isbuiltin(data);
+		//printf("mod = %i\n", mod);
+		if (mod == -1)
+		{
+			print_error(7);
+			printf("%s\n", data->builtins->command[0]);
+			ft_cleansplit(data->builtins->command);
+			return ;
+		}
+		data->builtins->funcs[mod - 1](data);
+		ft_cleansplit(data->builtins->command);
+	}
 }
