b1858144682897cd789121b4daabeacfdf9c8ea1
Knosper <79541185+Knosper@users.noreply.github.com>
2022-09-26 16:40:33 +0200

    rework .h

---
+++ src/tools/builtins.h
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjesberg <j.jesberger@heilbronn.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/08 13:42:25 by rmazurit          #+#    #+#             */
+/*   Updated: 2022/09/26 14:23:56 by jjesberg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUILTINS_H
+# define BUILTINS_H
+
+# include "structs.h"
+# include <errno.h>
+# include <unistd.h>
+# include <sys/types.h>
+
+//utils
+int		is_number(char *s);
+char 	**dl_first(char **s);
+char	*make_key(char *s, int *i);
+void	true_env(t_data *data);
+
+/*
+print data.envp.key & data.envp.key till envp->next == NULL
+if cmd:
+env | echo n 
+cmd env will be ignored atm
+*/
+int	env(t_data *data);
+
+/*
+data->envp->val = start path? or updated? 
+or cd() should update (data.val//data.val.next) 
+or an other fnct should do?
+export & unset env ?
+ARGS = ".."  & any path "Names"
+if error occurs return 1
+*/
+int cd(t_data *data);
+
+/*
+print the pathname of current working directory
+if error occurs return 1
+*/
+int pwd(void);
+
+/*
+flag echo -n dont work atm
+*/
+int	echo(t_data *data);
+
+/*
+not finished yet
+*/
+int	export(t_data *data);
+
+/*
+exit minishell should be fine
+pls test exit against bugs
+*/
+int	mini_exit(t_data *data);
+
+/*
+not finished yet
+*/
+int	unset(t_data *data);
+
+#endif
