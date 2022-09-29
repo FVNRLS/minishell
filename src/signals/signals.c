e44bbe6ef77300170131aa3cc1b16e4449c191d9
Knosper <79541185+Knosper@users.noreply.github.com>
2022-09-29 21:27:33 +0200

    Ctl_c signal

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -3,16 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rmazurit <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jjesberg <jjesberg@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 17:10:29 by rmazurit          #+#    #+#             */
-/*   Updated: 2022/09/23 17:11:43 by rmazurit         ###   ########.fr       */
+/*   Updated: 2022/09/29 21:26:04 by jjesberg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/minishell.h"
 
-int catch_signals(t_data *data)
+void catch_signals(int sig_num)
 {
-	return (EXIT_SUCCESS);
+	struct termios att;
+	struct termios saved;
+
+	tcgetattr(STDIN_FILENO, &saved);
+	tcsetattr(STDIN_FILENO, TCSANOW, &saved);
+	tcgetattr(STDIN_FILENO, &att);
+    att.c_lflag &= ~ ECHOCTL;
+    tcsetattr(STDIN_FILENO, 0, &att);
+	if (sig_num == SIGINT)
+	{
+		g_exit_code = 0;
+		if (ioctl(STDIN_FILENO, TIOCSTI, "\n"))
+			g_exit_code = printf("ERROR\n");
+		rl_on_new_line();
+		rl_replace_line("", 0);
+	}
+	
 }
