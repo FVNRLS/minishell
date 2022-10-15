31c968fbf494f3067e2e75a3f6d95a95b9f25e60
Roman Mazuritskiy <rmazurit@2-D-3.42heilbronn.de>
2022-10-15 19:02:31 +0200

    '' segfault fixed

--- src/tools/ft_get_builtin.c
+++ src/tools/ft_get_builtin.c
@@ -6,7 +6,7 @@
 /*   By: rmazurit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 14:37:17 by jjesberg          #+#    #+#             */
-/*   Updated: 2022/10/15 16:22:27 by rmazurit         ###   ########.fr       */
+/*   Updated: 2022/10/15 18:56:52 by rmazurit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,19 @@
 int	ft_get_builtin(t_data *data)
 {
 	data->builtins->command = ft_split(data->tokens->content, ' ');
-	if (ft_strcmp(data->builtins->command[0], data->builtins->names[0]) == 0)
+	if (ft_strcmp(data->exec->cmd[0], data->builtins->names[0]) == 0)
 		return (0);
-	if (ft_strcmp(data->builtins->command[0], data->builtins->names[1]) == 0)
+	if (ft_strcmp(data->exec->cmd[0], data->builtins->names[1]) == 0)
 		return (1);
-	if (ft_strcmp(data->builtins->command[0], data->builtins->names[2]) == 0)
+	if (ft_strcmp(data->exec->cmd[0], data->builtins->names[2]) == 0)
 		return (2);
-	if (ft_strcmp(data->builtins->command[0], data->builtins->names[3]) == 0)
+	if (ft_strcmp(data->exec->cmd[0], data->builtins->names[3]) == 0)
 		return (3);
-	if (ft_strcmp(data->builtins->command[0], data->builtins->names[4]) == 0)
+	if (ft_strcmp(data->exec->cmd[0], data->builtins->names[4]) == 0)
 		return (4);
-	if (ft_strcmp(data->builtins->command[0], data->builtins->names[5]) == 0)
+	if (ft_strcmp(data->exec->cmd[0], data->builtins->names[5]) == 0)
 		return (5);
-	if (ft_strcmp(data->builtins->command[0], data->builtins->names[6]) == 0)
+	if (ft_strcmp(data->exec->cmd[0], data->builtins->names[6]) == 0)
 		return (6);
 	return (-1);
 }
